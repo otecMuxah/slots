4d8da04d8c973bc9accd595f9a74fc8950723360
Misha <suxx_2000@hotmail.com>
2018-07-05 21:21:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     var validSlotItems = ['grape', 'lemon', 'orange', 'cherry', 'bell'];
-    var reelOne, reelTwo, reelThree;
-    function buildRandomArray(sourceArray, length) {
+    var numberOfReels = 3;
+    var reelArray = [];
+    var buildRandomArray = function (sourceArray, length) {
         var range = [];
         range.length = length;
         var tempArray = [];
@@ -11,45 +12,65 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         ;
         return tempArray;
-    }
-    reelOne = buildRandomArray(validSlotItems, 20);
-    reelTwo = buildRandomArray(validSlotItems, 25);
-    reelThree = buildRandomArray(validSlotItems, 30);
-    var currentItems = [reelOne[0],
-        reelTwo[0],
-        reelThree[0],
-        reelOne[1],
-        reelTwo[1],
-        reelThree[1],
-        reelOne[2],
-        reelTwo[2],
-        reelThree[2]
+    };
+    var buildReel = function (numberOfArrays) {
+        var i = 0;
+        var numberOfFrames = 20;
+        while (i < numberOfArrays) {
+            reelArray[i] = buildRandomArray(validSlotItems, numberOfFrames);
+            numberOfFrames += 5;
+            i++;
+        }
+    };
+    buildReel(numberOfReels);
+    var currentItems = [
+        reelArray[0][0],
+        reelArray[1][0],
+        reelArray[2][0],
+        reelArray[0][1],
+        reelArray[1][1],
+        reelArray[2][1],
+        reelArray[0][2],
+        reelArray[1][2],
+        reelArray[2][2]
     ];
-    function renderReel(source) {
+    var renderReel = function (source) {
         var tempString = '';
         for (var _i = 0, source_1 = source; _i < source_1.length; _i++) {
             var item = source_1[_i];
             tempString += "<li class=\"slot\"><div class=\"" + item + "\"></div></li>";
         }
         return "<ul class=\"reel\">" + tempString + "</ul>";
-    }
-    var tempHTML = renderReel(reelOne) + renderReel(reelTwo) + renderReel(reelThree);
+    };
+    var tempHTML = renderReel(reelArray[0]) + renderReel(reelArray[1]) + renderReel(reelArray[2]);
     var app = document.getElementById('app');
     app.innerHTML = tempHTML;
     var spinButton = document.getElementById('spin');
-    spinButton.addEventListener('click', function () {
+    var animateReels = function () {
         var reels = document.getElementsByClassName('reel');
+        // FIXME - [...reels].forEach not working for some reason
         Array.prototype.forEach.call(reels, function (el) {
-            console.log(el.clientHeight);
-            el.style.transition = "ease-out " + (el.clientHeight - 450) / 1000 + "s";
-            var translate = "translate(0, -" + (el.clientHeight - 450) + "px)";
+            el.style.transition = "none";
+            var translate = "translate(0, 0)";
+            el.style.transform = translate;
+            var elementHeight = el.clientHeight;
+            el.style.transition = "ease-out " + (elementHeight - 450) / 1000 + "s";
+            translate = "translate(0, -" + (elementHeight - 450) + "px)";
             el.style.transform = translate;
         });
-    });
+    };
+    spinButton.addEventListener('click', animateReels);
     var inputs = document.getElementsByClassName('input');
-    var index = 0;
-    Array.prototype.forEach.call(inputs, function (el, i) {
-        console.log(el, i);
-        el.value = currentItems[i];
-    });
+    var getCurrentSlots = function () {
+        Array.prototype.forEach.call(inputs, function (el, i) {
+            el.value = currentItems[i];
+        });
+    };
+    var setCurrentSlots = function () {
+        Array.prototype.forEach.call(inputs, function (el, i) {
+            for (var i_1 = 0; i_1 < numberOfReels; i_1++) {
+            }
+        });
+    };
+    getCurrentSlots();
 });
