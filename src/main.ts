33af6cd18fd43c31b4d5534f3e9c62d5823e4bfa
Misha <suxx_2000@hotmail.com>
2018-07-12 00:25:09 +0300

    seems good

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { SlotsGame } from './slotsGame'
 
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    const slots = new SlotsGame(3,3, document.getElementById('app'));
+});
 
